92f54e4dfda5e833a3dfdad297abbcfa109b3ba6
mbspeczek <mbspeczek@gmail.com>
2021-11-12 19:01:08 +0100

    PostgreSQL database and configuration added.

--- PyTaskManager/settings.py
+++ PyTaskManager/settings.py
@@ -72,12 +72,17 @@ WSGI_APPLICATION = 'PyTaskManager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+# default for postgre database
+# needed packages: postgresql - as server, psycopg2 - for connections
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'djangodb',
+       'USER': 'dbuser',
+       'PASSWORD': 'Root1234',
+       'HOST': 'localhost',
+       'PORT': '',
+   }
 }
 
 
